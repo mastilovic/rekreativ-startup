7dc84ee6b9365c4f9162290a091bd789391397c7
salexdxd <aleksandar96.m@gmail.com>
2022-04-09 16:12:47 +0200

    Deleted: Unused files

--- src/main/java/com/example/RekreativStartup/controller/MatchupController.java
+++
@@ -1,4 +0,0 @@
-//package com.example.RekreativStartup.controller;
-//
-//public class MatchupController {
-//}
--- src/main/java/com/example/RekreativStartup/repository/MatchupRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.example.RekreativStartup.repository;
-//
-//import com.example.RekreativStartup.model.Matchup;
-//import org.springframework.data.repository.PagingAndSortingRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface MatchupRepository extends PagingAndSortingRepository<Matchup, Long> {
-//}
